5280d4152ace4035620f432a94513db1cec8397b
SurajGupta2k <surajgupta3738@gmail.com>
2025-03-03 16:33:04 +0530

    removed stripe

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -7,33 +7,23 @@ const orderSchema = new mongoose.Schema(
 			ref: "User",
 			required: true,
 		},
-		products: [
-			{
-				product: {
-					type: mongoose.Schema.Types.ObjectId,
-					ref: "Product",
-					required: true,
-				},
-				quantity: {
-					type: Number,
-					required: true,
-					min: 1,
-				},
-				price: {
-					type: Number,
-					required: true,
-					min: 0,
-				},
-			},
-		],
+		product: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Product",
+			required: true,
+		},
+		coupon: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Coupon",
+		},
+		status: {
+			type: String,
+			enum: ["pending", "completed", "cancelled"],
+			default: "pending",
+		},
 		totalAmount: {
 			type: Number,
 			required: true,
-			min: 0,
-		},
-		stripeSessionId: {
-			type: String,
-			unique: true,
 		},
 	},
 	{ timestamps: true }
