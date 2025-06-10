3a6a28c3cb21a833d730a688f49834bc4c44c837
SurajGupta2k <surajgupta3738@gmail.com>
2025-06-10 16:19:16 +0530

    Update payment.route.js

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { checkoutSuccess, createCheckoutSession } from "../controllers/payment.controller.js";
+import { createCheckoutSession, handleWebhook, getPaymentStatus } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
-router.post("/checkout-success", protectRoute, checkoutSuccess);
+router.post("/webhook", handleWebhook);
+router.get("/status/:orderId", protectRoute, getPaymentStatus);
 
 export default router;
